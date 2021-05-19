61ef75ee38507e75305fd359675e16e32a89bf4f
Harley White <acamaeda@gmail.com>
2021-05-19 12:30:02 -0400

    Fixed things not updating

--- js/technical/temp.js
+++ js/technical/temp.js
@@ -113,8 +113,8 @@ function updateTempData(layerData, tmpData, funcsData) {
 	
 	for (item in funcsData){
 		if (Array.isArray(layerData[item])) {
-			if (item === "tabFormat" || item === "content") return // These are only updated when needed
-			updateTempData(layerData[item], tmpData[item], funcsData[item])
+			if (item !== "tabFormat" && item !== "content") // These are only updated when needed
+				updateTempData(layerData[item], tmpData[item], funcsData[item])
 		}
 		else if ((!!layerData[item]) && (layerData[item].constructor === Object) || (typeof layerData[item] === "object") && traversableClasses.includes(layerData[item].constructor.name)){
 			updateTempData(layerData[item], tmpData[item], funcsData[item])
