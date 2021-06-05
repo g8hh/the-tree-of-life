cbcbd1d0e5b79dbd6f77aa526e7c8caeb69b512a
Harley White <acamaeda@gmail.com>
2021-06-04 20:37:03 -0400

    Removed NaN checks from temp

--- js/technical/temp.js
+++ js/technical/temp.js
@@ -92,34 +92,17 @@ function updateTemp() {
 	updateTempData(layers, tmp, funcs)
 
 	for (layer in layers){
-		let problem = ""
 		tmp[layer].resetGain = getResetGain(layer)
-		if (checkDecimalNaN(tmp[layer].resetGain))
-			problem = "resetGain"
 		tmp[layer].nextAt = getNextAt(layer)
-		if (checkDecimalNaN(tmp[layer].nextAt))
-			problem = "nextAt"
 		tmp[layer].nextAtDisp = getNextAt(layer, true)
 		tmp[layer].canReset = canReset(layer)
 		tmp[layer].trueGlowColor = tmp[layer].glowColor
 		tmp[layer].notify = shouldNotify(layer)
 		tmp[layer].prestigeNotify = prestigeNotify(layer)
-		if (problem && !NaNalert) {
-			confirm("Invalid value found in temp." + layer + "." + problem + ". Please let the creator of this mod know! You can refresh the page, and you will be un-NaNed.")
-			clearInterval(interval);
-			NaNalert = true;
-			return
-		}
 
 	}
 
 	tmp.pointGen = getPointGen()
-	if (checkDecimalNaN(tmp.pointGen) && !NaNalert) {
-		confirm("Invalid value found in temp.pointGen. Please let the creator of this mod know! You can refresh the page, and you will be un-NaNed.")
-		clearInterval(interval);
-		NaNalert = true;
-		return
-	}
 	tmp.displayThings = []
 	for (thing in displayThings){
 		let text = displayThings[thing]
@@ -142,14 +125,6 @@ function updateTempData(layerData, tmpData, funcsData, useThis) {
 
 			if (useThis !== undefined) value = layerData[item].bind(useThis)()
 			else value = layerData[item]()
-			if (value !== value || checkDecimalNaN(value)){
-				if (!NaNalert) {
-					confirm("Invalid value found in tmp, named '" + item + "'. Please let the creator of this mod know! You can refresh the page, and you will be un-NaNed.")
-					clearInterval(interval);
-					NaNalert = true;
-					return
-				}
-			}
 			Vue.set(tmpData, item, value)
 		}
 	}	
