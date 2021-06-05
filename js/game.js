4bc8b7712aaec7d518a553d2405f97c7b2c6eb1a
Harley White <acamaeda@gmail.com>
2021-06-04 20:38:05 -0400

    2.6.0.1

--- js/game.js
+++ js/game.js
@@ -4,8 +4,8 @@ var gameEnded = false;
 
 // Don't change this
 const TMT_VERSION = {
-	tmtNum: "2.6",
-	tmtName: "Dreams Really Do Come True"
+	tmtNum: "2.6.0.1",
+	tmtName: "Fixed Reality"
 }
 
 function getResetGain(layer, useType = null) {
