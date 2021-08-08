fc2b4fa24bd3332c5102ce98e9482545a5bf53a4
pg132 <picogilman@gmail.com>
2021-08-08 07:50:24 -0700

    v.100 fix

--- js/layers.js
+++ js/layers.js
@@ -21662,7 +21662,7 @@ addLayer("mc", {
                         },
                 },
         },
-        layerShown(){return hasMilestone("cells", 21) || player.o.unlocked},
+        layerShown(){return hasMilestone("cells", 21) || player.or.unlocked},
         buyables: {
                 rows: 3,
                 cols: 3,
