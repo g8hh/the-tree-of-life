cfc3f767ba1fc06c5f51473ef1347336cdcfef07
pg132 <picogilman@gmail.com>
2021-08-17 08:47:32 -0400

    v.107.1

--- js/mod.js
+++ js/mod.js
@@ -12,7 +12,7 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "1.107",
+	num: "1.107.1",
 	name: "Advil's Auspicious Acension",
 }
 
@@ -33,6 +33,10 @@ let changelog = `<h1>Changelog:</h1><br>
 		- B will be each content patch.<br>
 		- C will be small patches without content (bug/wording fixes).<br><br><br>
 
+	<br><h3 style='color: #CC0000'>v1.107.1</h3><br>
+		- Fixed a bug with Cell production.<br>
+		- Added a custom save.<br>
+		- Fixed various display issues.<br>
 	<br><h3 style='color: #CC0000'>v1.107</h3><br>
 		- Added a custom save.<br>
 		- Fixed extreme mode description.<br>
