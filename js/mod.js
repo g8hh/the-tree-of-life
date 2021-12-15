5f6b71f01f65fe9dbff85078536f270086de942c
pg132 <picogilman@gmail.com>
2021-12-14 22:05:08 -0800

    v.217.1

--- js/mod.js
+++ js/mod.js
@@ -12,7 +12,7 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "1.217",
+	num: "1.217.1",
 	name: "Advil's Auspicious Acension",
 }
 
@@ -26,13 +26,16 @@ function isEndgame() {
 
 let changelog = `<h1>Changelog:</h1><br>
 	<br><h2 style='color: #DDDD00'>Endgame:</h2><br>
-		Reaching the endgame screen (updated at least as of v1.217)<br><br>
+		Reaching the endgame screen (updated at least as of v1.217.1)<br><br>
 	<br><h2 style='color: #00CC00'>Notes</h2><br>
 		- Versions will be vA.B.C<br>
 		- A will be big releases.<br>
 		- B will be each content patch.<br>
 		- C will be small patches without content (bug/wording fixes).<br><br><br>
 
+	<br><h3 style='color: #CC0000'>v1.217.1</h3><br>
+		- Added five rows of achievements.<br>
+		- Made achievements unlock at various points.<br>
 	<br><h3 style='color: #CC0000'>v1.217</h3><br>
 		- Balanced until 1e100 Organs.<br>
 		- Added 3 Organ upgrades.<br>
